2af38ef6ae79d3d268f548c50ec5b57c5a4dcdcd
Gaurav Matta <gaurav.matta.tech@gmail.com>
2024-02-18 02:47:18 +0530

    updated path of jet transformation Project

--- simple_mail_transform/jet2java/simple/mail/transform/compiled/_jet_letterhtml.java
+++ simple_mail_transform/jet2java/simple/mail/transform/compiled/_jet_letterhtml.java
@@ -3,26 +3,14 @@ package simple.mail.transform.compiled;
 import org.eclipse.jet.JET2Context;
 import org.eclipse.jet.JET2Template;
 import org.eclipse.jet.JET2Writer;
-import org.eclipse.jet.taglib.RuntimeTagElement;
-import org.eclipse.jet.taglib.TagInfo;
 
 public class _jet_letterhtml implements JET2Template {
-    private static final String _jetns_c = "org.eclipse.jet.controlTags"; //$NON-NLS-1$
 
     public _jet_letterhtml() {
         super();
     }
 
     private static final String NL = System.getProperty("line.separator"); //$NON-NLS-1$
-    
-    private static final TagInfo _td_c_get_20_9 = new TagInfo("c:get", //$NON-NLS-1$
-            20, 9,
-            new String[] {
-                "select", //$NON-NLS-1$
-            },
-            new String[] {
-                "$mailing/@firstName", //$NON-NLS-1$
-            } );
 
     public void generate(final JET2Context context, final JET2Writer __out) {
         JET2Writer out = __out;
@@ -63,13 +51,7 @@ public class _jet_letterhtml implements JET2Template {
         out.write(NL);         
         out.write("<div class=\"container\">");  //$NON-NLS-1$        
         out.write(NL);         
-        out.write("<p>Dear ");  //$NON-NLS-1$        
-        RuntimeTagElement _jettag_c_get_20_9 = context.getTagFactory().createRuntimeTag(_jetns_c, "get", "c:get", _td_c_get_20_9); //$NON-NLS-1$ //$NON-NLS-2$
-        _jettag_c_get_20_9.setRuntimeParent(null);
-        _jettag_c_get_20_9.setTagInfo(_td_c_get_20_9);
-        _jettag_c_get_20_9.doStart(context, out);
-        _jettag_c_get_20_9.doEnd();
-        out.write("</p>");  //$NON-NLS-1$        
+        out.write("<p>Dear Gaurav</p>");  //$NON-NLS-1$        
         out.write(NL);         
         out.write("<p>Thanks for Subscribing</p>");  //$NON-NLS-1$        
         out.write(NL);         
---
+++ simple_mail_transform/jet2java/simple/mail/transform/compiled/_jet_project.java
@@ -0,0 +1,30 @@
+package simple.mail.transform.compiled;
+
+import org.eclipse.jet.JET2Context;
+import org.eclipse.jet.JET2Template;
+import org.eclipse.jet.JET2Writer;
+
+public class _jet_project implements JET2Template {
+
+
+/*
+ *+------------------------------------------------------------------------+
+ *| Licensed Materials - Property of IBM                                   |
+ *| (C) Copyright IBM Corp. 2006.  All Rights Reserved.                    |
+ *|                                                                        |
+ *| US Government Users Restricted Rights - Use, duplication or disclosure |
+ *| restricted by GSA ADP Schedule Contract with IBM Corp.                 |
+ *+------------------------------------------------------------------------+
+ */
+
+    public _jet_project() {
+        super();
+    }
+
+    public void generate(final JET2Context context, final JET2Writer __out) {
+        JET2Writer out = __out;
+        //
+        //The action corresponding to this template had no associated exemplar references.
+        //Consequently, the Authoring Editor was unable to generate any meaningful content.
+    }
+}
